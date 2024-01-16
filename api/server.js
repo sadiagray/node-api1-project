1c764254130a7f28fa1ec4487cd8eb19409ae49c
Sadia Gray <sadiagray@Sadias-MBP.home>
2024-01-15 23:10:50 -0600

    fompleted what i could of mvp, possible bugs bc even solution video could not pass final tests

--- api/server.js
+++ api/server.js
@@ -67,43 +67,49 @@ server.use('*', (req, res) => {
 
 server.put('/api/users/:id', async(req,res) => {
     try {
-        const { id } = req.params
-        const { name, bio } = req.body
-        if(!name||!bio){
-            res.status(400).json({
-                message: "Please provide name and bio for the user"
+        const possibleUser = await User.findById(req.params.id)
+        if(!possibleUser){
+            res.status(404).json({
+                message: "The user with the specified ID does not exist"
             })
         }else{
-            const updatedUser = await User.update(id, { name, bio })
-            if(!updatedUser){
-                res.status(404).json({
-                    message: "The user with the specified ID does not exist"
+            if(!req.body.name || !req.body.bio){
+                res.status(400).json({
+                    message: "Please provide name and bio for the user"
                 })
             }else{
+                const updatedUser = await User.update(
+                    req.params.id,
+                    req.body,
+                )
                 res.status(200).json(updatedUser)
             }
         }
     } catch (error) {
         res.status(500).json({
-            message: 'The user information could not be modified'
+            message: 'The user information could not be modified',
+            err: err.message,
+            stack: err.stack,
         })
     }
 })
 
 server.delete('/api/users/:id', async(req,res) => {
     try {
-        const { id } = req.params
-        const deletedUser = await User.remove(id)
-        if(!deletedUser){
+        const possibleUser = await User.findById(req.params.id)
+        if(!possibleUser){
             res.status(404).json({
                 message: "The user with the specified ID does not exist"
             })
         }else{
+            const deletedUser = await User.remove(possibleUser.id)
             res.status(200).json(deletedUser)
         }
     } catch (error) {
         res.status(500).json({
-            message: "The user could not be removed"
+            message: "The user could not be removed",
+            err: err.message,
+            stack: err.stack,
         })
     }
 })
